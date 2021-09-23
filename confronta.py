a5ca11c8fb930b982d1a1665af7b2adf8ec9ea82
Rodolfo Carobene <rodolfo.carobene@gmail.com>
2021-09-23 18:07:07 +0200

    style

--- confronta.py
+++ confronta.py
@@ -4,49 +4,18 @@ import sys
 
 import numpy as np
 
+from subroutineMAIN import return_classical_results
+
 def import_valori_veri(dists, mol_type):
     all_dists = np.arange(0.3, 3.5, 0.1)
     riferimento_import = []
 
-    if mol_type == 'H2*':
-        vals_import = [0.8746663352974902, 0.28351783858747615,
-                       -0.07984820956276151, -0.3201959251257661,
-                       -0.48669833531774864, -0.6055310045065269,
-                       -0.6919856041885428, -0.7556938311464096,
-                       -0.8030523503481138, -0.8384549893848645,
-                       -0.8649961002491231, -0.8849048747158421,
-                       -0.8998201809754169, -0.9109662272337082,
-                       -0.9192672637162707, -0.925425076771708,
-                       -0.9299734637128507, -0.9333180090970401,
-                       -0.9357660130931245, -0.9375494300790719,
-                       -0.9388425886811848, -0.9397758952469387,
-                       -0.9404464113188682, -0.9409259963579195,
-                       -0.9412675574323823, -0.9415098285613728,
-                       -0.9416810086568391, -0.9418015147949865,
-                       -0.9418860522452043, -0.9419451598079606,
-                       -0.9419863551299065, -0.942014977507915]
-    else:
-        vals_import = [0.7930249944681114, -0.23748138680009934,
-                       -0.7527805858030638, -1.0251057217763133,
-                       -1.1699429398436911, -1.2433092266100079,
-                       -1.2748268385778982, -1.2811879994251272,
-                       -1.2722647705355448, -1.2541321018359217,
-                       -1.2306719129030712, -1.2044585772543404,
-                       -1.1772556560041034, -1.1502958922569917,
-                       -1.1244398972305523, -1.1002689580616154,
-                       -1.0781444800233184, -1.0582520399914663,
-                       -1.0406387409179958, -1.0252471289480645,
-                       -1.0119461452363505, -1.000558408005646,
-                       -0.990882909320383, -0.9827125641869094,
-                       -0.9758466366305577, -0.9700986094113239,
-                       -0.965300395780803, -0.9613038824951535,
-                       -0.9579807036602958, -0.9552209634214398,
-                       -0.9529314255228293, -0.9510335121611919]
+    vals_import = return_classical_results(mol_type)
 
     list_tupla = zip(all_dists, vals_import)
 
-    for dist, energy in list_tupla:
-        if np.round(dist, 2) in [np.round(num, 2) for num in dists]:
+    for distanza, energy in list_tupla:
+        if np.round(distanza, 2) in [np.round(num, 2) for num in dists]:
             riferimento_import.append(energy)
 
     return riferimento_import
@@ -56,14 +25,14 @@ def valori_da_confrontare(filename):
     data = json.load(file)
     file.close()
 
-    vals = {}
+    vals_final = {}
     for res in data['results_tot']:
-        y = []
-        for singleResult in data['results_tot'][res]:
-            y.append(singleResult['energy'])
-        vals[res] = y
+        ax_y = []
+        for single_result in data['results_tot'][res]:
+            ax_y.append(single_result['energy'])
+        vals_final[res] = ax_y
 
-    return vals, data['options']['dists'], data['options']['molecule']['molecule']
+    return vals_final, data['options']['dists'], data['options']['molecule']['molecule']
 
 def sort_dict(dict1):
     newdict = {k: v for k, v in sorted(dict1.items(),
@@ -76,7 +45,7 @@ def sort_dict(dict1):
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         print('missing args')
-        exit()
+        sys.exit()
 
     vals, dists, mol_type = valori_da_confrontare(sys.argv[1])
     riferimento = import_valori_veri(dists, mol_type)
