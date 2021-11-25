8a613aa1f37f4bbdad64460890bfc6ff378852fe
Rodolfo Carobene <rodolfo.carobene@gmail.com>
2021-11-25 11:15:54 +0100

    solved merge problem

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name             = 'spvqe',
     version          = '0.0.1',
     description      = 'Package to study the Series of Penalty VQE variation',
-    long_description = read('README.md'),
+    long_description = read('README.MD'),
     author           = 'Rodolfo Carobene',
     author_email     = 'r.carobene@campus.unimib.it',
     url              = 'https://github.com/rodolfocarobene/spsvqe',
